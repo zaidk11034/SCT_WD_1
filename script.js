04db0d8e39cdf0acf4645e9497d5ebbdf8ae6f19
Khan Zaid <khanzaid11034@gmail.com>
2024-06-05 20:15:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 60) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
